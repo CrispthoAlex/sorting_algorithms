9d7e864a62b63778c1021906a5f0014c98331da2
crispthoalex <alex877@gmail.com>
2020-06-12 23:36:38 +0000

    Add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - algorithm to sort
+ * @array: our array unsorted to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t pass = 0, offset = 0;
+
+	if (size < 2 || !array)
+		return;
+
+	for (; pass < size; pass++)
+	{
+		offset = 0;
+		for (; offset < size - 1 - pass; offset++)
+		{
+			if (array[offset] > array[1 + offset])
+			{
+				swap_int(&array[offset], &array[1 + offset]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+
+/**
+ * swap_int -  swap two values/int
+ * @value1: value 1 to swap with value 2
+ * @value2: value 2 to swap with value 1
+ */
+void swap_int(int *value1, int *value2)
+{
+	*value1 = *value1 ^ *value2;
+	*value2 = *value2 ^ *value1;
+	*value1 = *value1 ^ *value2;
+}
--- 0-main.c
+++ 0-main.c
@@ -10,12 +10,41 @@
 int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array1[] = {19}; /*int array2 = NULL;*/
+    int array3[] = {5,4};
     size_t n = sizeof(array) / sizeof(array[0]);
+    size_t n1 = 1, n2 = 5, n3 = 2;
 
     print_array(array, n);
     printf("\n");
     bubble_sort(array, n);
     printf("\n");
     print_array(array, n);
+
+    printf("----------------------------\n");
+
+    print_array(array1, n1);
+    printf("\n");
+    bubble_sort(array1, n1);
+    printf("\n");
+    print_array(array1, n1);
+
+
+    printf("----------------------------\n");
+
+    print_array(NULL, n2);
+    printf("\n");
+    bubble_sort(NULL, n2);
+    printf("\n");
+    print_array(NULL, n2);
+
+    printf("----------------------------\n");
+
+    print_array(array3, n3);
+    printf("\n");
+    bubble_sort(array3, n3);
+    printf("\n");
+    print_array(array3, n3);
+
     return (0);
 }
