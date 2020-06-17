ad6c178a2c29426e2ca10a26a42b96de0ec8718a
daorejuela1 <vids@hotmail.es>
2020-06-17 21:30:32 +0000

    updated advance sorting algorithms templates

---
+++ 1000-sort_deck.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * sort_deck - sorts an array of integers with shell sorting
+ *
+ * @deck: linked list with decks
+ * Return: Void
+ */
+void sort_deck(deck_node_t **deck)
+{
+	if (deck == NULL || *deck == NULL)
+		return;
+}
+
+
