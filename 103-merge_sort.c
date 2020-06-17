397cb9a18ddc144a3dd4576c477e25a528109180
daorejuela1 <vids@hotmail.es>
2020-06-17 21:12:34 +0000

    uploaded merge sort test

---
+++ 103-merge_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array of integers with merge sorting
+ *
+ * @array: array
+ * @size: size of the array
+ * Return: Void
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (size < 2 || !array)
+		return;
+}
+
+
--- sort.h
+++ sort.h
@@ -33,4 +33,9 @@ void shell_sort(int *array, size_t size);
 int partition(int *array, size_t size, int low, int high);
 
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif /* END SORT.H */
